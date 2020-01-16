08aabe6a4e861cec9afc80e6a8fc21b80f2b30b7
Floriani Gianfranco <Gianfranco Floriani@PNet.it>
2020-01-16 15:48:37 +0100

    GF - Added new Vue line
    Now there is a span ...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var gfapp = new Vue({
     el: '#gfapp',
     data: {
-      gfmessage: 'Hello GF-Vue!'
+      gfmessage: 'Hello GF-Vue!',
+      gfmessage2: 'You loaded this page on ' + new Date().toLocaleString()
     }
   })
